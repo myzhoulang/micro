5997629b64d26db5ff621539e8b4c005b6056619
zhoulang <lang.zhou1@geely.com>
2021-12-16 17:41:39 +0800

    add webpackBundleAnalyzer plugin

--- umi/.umirc.ts
+++ umi/.umirc.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'umi';
 import { name } from './package.json';
-import path from 'path'
+import BundleAnalyzerPlugin from 'webpack-bundle-analyzer'
 
 console.log(name);
 export default defineConfig({
+  chainWebpack(memo, { env, webpack, createCSSRule }) {
+    // // 设置 alias
+    // memo.resolve.alias.set('foo', '/tmp/a/b/foo');
+
+    // // 删除 umi 内置插件
+    // memo.plugins.delete('progress');
+    // memo.plugins.delete('friendly-error');
+    // memo.plugins.delete('copy');
+    memo.plugin('bundle-analyzer').use(BundleAnalyzerPlugin.BundleAnalyzerPlugin)
+  },
   base: name,
   devServer: {
     port: 8002,
